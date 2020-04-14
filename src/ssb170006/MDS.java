bd025561abae03e12fbbfb67c4662166e2337876
Sankalp Bhandari <sankalp.bhandari@gmail.com>
2020-04-14 12:39:05 -0500

    Added DocString

--- src/ssb170006/MDS.java
+++ src/ssb170006/MDS.java
@@ -15,12 +15,12 @@ import java.util.*;
 
 
 public class MDS {
-    TreeMap<Long, Product> itemMap; // Map of item id and item
-    HashMap<Long, HashSet<Long>> nameMap; // Map of description and item ids
+    TreeMap<Long, Product> productMap; // Map of product id and product
+    HashMap<Long, HashSet<Long>> nameMap; // Map of name and product ids
 
     // Constructors
     public MDS() {
-        itemMap = new TreeMap<>();
+        productMap = new TreeMap<>();
         nameMap = new HashMap<>();
     }
 
@@ -32,14 +32,25 @@ public class MDS {
        is null or empty, in which case, just the price is updated. 
        Returns 1 if the item is new, and 0 otherwise.
     */
+
+    /**
+     * Insert a new product whose name is given in the list.
+     * If an entry with the same id already exists, then its name and price are replaced
+     * by the new values, unless list is null or empty, in which case, just the price is updated.
+     *
+     * @param id    id of product
+     * @param price price of product
+     * @param list  the list containing the name of the product
+     * @return 1 if the product is new, and 0 otherwise.
+     **/
     public int insert(long id, Money price, List<Long> list) {
-        if (!itemMap.containsKey(id)) {
+        if (!productMap.containsKey(id)) {
             Product product = new Product(id, price, list);
-            itemMap.put(id, product);
+            productMap.put(id, product);
             updateNameMap(id);
             return 1;
         }
-        Product oldProduct = itemMap.get(id);
+        Product oldProduct = productMap.get(id);
         if (list != null && list.size() > 0) {
             removeNames(id, new ArrayList<>(oldProduct.name));
             oldProduct.name.clear();
@@ -51,24 +62,42 @@ public class MDS {
 
     }
 
+    /**
+     * Updates the Name Map that is the reverse mapping of the product to names.
+     *
+     * @param id id of product.
+     */
     private void updateNameMap(long id) {
         Product product = findProduct(id);
         for (long desc : product.name) {
             if (nameMap.containsKey(desc)) {
                 nameMap.get(desc).add(id);
             } else {
-                HashSet<Long> itemIds = new HashSet<>();
-                itemIds.add(id);
-                nameMap.put(desc, itemIds);
+                HashSet<Long> prodIds = new HashSet<>();
+                prodIds.add(id);
+                nameMap.put(desc, prodIds);
             }
         }
     }
 
+    /**
+     * Finds product of given id
+     *
+     * @param id id of product
+     * @return the Product if it is found otherwise null
+     **/
     private Product findProduct(long id) {
-        return itemMap.getOrDefault(id, null);
+        return productMap.getOrDefault(id, null);
     }
 
     // b. Find(id): return price of item with given id (or 0, if not found).
+
+    /**
+     * Finds the price of product given it's id
+     *
+     * @param id id of product
+     * @return price of product with given id or 0, if not found
+     **/
     public Money find(long id) {
         Product product = findProduct(id);
         if (product != null)
@@ -81,6 +110,14 @@ public class MDS {
        long ints that are in the description of the item deleted,
        or 0, if such an id did not exist.
     */
+
+    /**
+     * Delete product from storage
+     *
+     * @param id id of product to be deleted
+     * @return the sum of the names that are in the name of the
+     * product deleted, or 0, if such an id did not exist.
+     **/
     public long delete(long id) {
         Product product = findProduct(id);
         if (null == product)
@@ -92,7 +129,7 @@ public class MDS {
             if (nameMap.get(desc).isEmpty())
                 nameMap.remove(desc);
         }
-        itemMap.remove(id);
+        productMap.remove(id);
         return sum;
     }
 
@@ -102,12 +139,20 @@ public class MDS {
        item's description), and return lowest price of those items.
        Return 0 if there is no such item.
     */
+
+    /**
+     * Given a long int, find products whose name
+     * contains that number, and return lowest price of those products.
+     *
+     * @param n the long int to search for in the product names
+     * @return lowest price of the product. Return 0 if there is no such product.
+     **/
     public Money findMinPrice(long n) {
         if (!nameMap.containsKey(n))
             return new Money();
-        HashSet<Long> itemIds = nameMap.get(n);
+        HashSet<Long> prodIds = nameMap.get(n);
         Money minPrice = convertToMoney(Long.MAX_VALUE);
-        for (long id : itemIds) {
+        for (long id : prodIds) {
             Product tempProduct = findProduct(id);
             if (tempProduct.price.compareTo(minPrice) < 0) {
                 minPrice = tempProduct.price;
@@ -121,12 +166,20 @@ public class MDS {
        contains that number, and return highest price of those items.
        Return 0 if there is no such item.
     */
+
+    /**
+     * Given a long int, find products whose name
+     * contains that number, and return highest price of those products.
+     *
+     * @param n the long int to search for in the product name
+     * @return highest price of the products. Return 0 if there is no such product.
+     **/
     public Money findMaxPrice(long n) {
         if (!nameMap.containsKey(n))
             return new Money();
-        HashSet<Long> itemIds = nameMap.get(n);
+        HashSet<Long> prodIds = nameMap.get(n);
         Money maxPrice = new Money();
-        for (long id : itemIds) {
+        for (long id : prodIds) {
             Product tempProduct = findProduct(id);
             if (tempProduct.price.compareTo(maxPrice) > 0) {
                 maxPrice = tempProduct.price;
@@ -140,6 +193,16 @@ public class MDS {
        of items whose description contains n, and in addition,
        their prices fall within the given range, [low, high].
     */
+
+    /**
+     * Given a long int, find the number of products whose name contains n,
+     * and their prices fall within a given range
+     *
+     * @param n    the long int to search for in the product names
+     * @param low  the low end of the price range, inclusive
+     * @param high the high end of the price range, inclusive
+     * @return the number of products in this price range
+     **/
     public int findPriceRange(long n, Money low, Money high) {
         if (!nameMap.containsKey(n))
             return 0;
@@ -147,8 +210,8 @@ public class MDS {
             return 0;
 
         int count = 0;
-        HashSet<Long> itemIds = nameMap.get(n);
-        for (long id : itemIds) {
+        HashSet<Long> prodIds = nameMap.get(n);
+        for (long id : prodIds) {
             Product tempProduct = findProduct(id);
             if (tempProduct.price.compareTo(low) >= 0 && tempProduct.price.compareTo(high) <= 0)
                 count += 1;
@@ -161,11 +224,21 @@ public class MDS {
        in the range [l,h] by r%.  Discard any fractional pennies in the new
        prices of items.  Returns the sum of the net increases of the prices.
     */
+
+    /**
+     * Increase the price of every product, whose id is in a range by specific percentage.
+     * Discard any fractional pennies in the new prices of products.
+     *
+     * @param l    the low end of the ID range, inclusive
+     * @param h    the high end of the ID range, inclusive
+     * @param rate the percentage rate to increase prices
+     * @return the sum of the net increases of the prices
+     **/
     public Money priceHike(long l, long h, double rate) {
-        if (itemMap.firstKey().compareTo(h) > 0 || l > h || itemMap.lastKey().compareTo(l) < 0)
+        if (productMap.firstKey().compareTo(h) > 0 || l > h || productMap.lastKey().compareTo(l) < 0)
             return new Money();
         long hike = 0;
-        for (long id : itemMap.keySet()) {
+        for (long id : productMap.keySet()) {
             if (id < l)
                 continue;
             if (id > h)
@@ -180,10 +253,22 @@ public class MDS {
         return convertToMoney(hike);
     }
 
+    /**
+     * Converts Money to Long
+     *
+     * @param money the Money to be converted
+     * @return the Long value, in cents
+     **/
     private Long convertToLong(Money money) {
         return money.dollars() * 100 + money.cents();
     }
 
+    /**
+     * Converts Long to Money
+     *
+     * @param money the Long value, in cents, that is to be converted
+     * @return the Money
+     **/
     private Money convertToMoney(long money) {
         int cents = (int) (money % 100);
         long dollars = money / 100;
@@ -196,6 +281,16 @@ public class MDS {
       id's description.  Return the sum of the numbers that are actually
       deleted from the description of id.  Return 0 if there is no such id.
     */
+
+    /**
+     * Remove elements of list from the name of id.
+     * It is possible that some of the products in the list are not in the id's name.
+     *
+     * @param id   id of product
+     * @param list list of names to be removed
+     * @return the sum of the numbers that are actually deleted from the name of id.
+     * Return 0 if there is no such id.
+     **/
     public long removeNames(long id, List<Long> list) {
         if (null == list || list.isEmpty())
             return 0;
